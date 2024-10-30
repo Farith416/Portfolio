c8652aadf27324d2d8a463339a92541c9cebd9c5
Farith Ahamed <Cleverboyfarith@gmail.com>
2024-10-30 21:16:11 +0530

    Initial Commit

--- main.js
+++ main.js
@@ -27,3 +27,37 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    let isValid = true;
+    
+    const firstName = document.getElementById("Fname");
+    const lastName = document.getElementById("Lname");
+    const phoneNumber = document.getElementById("num");
+    const email = document.getElementById("email");
+    const message = document.getElementById("msg");
+
+    if (!firstName.value.trim()) {
+        alert("First Name is required.");
+        isValid = false;
+    } else if (!lastName.value.trim()) {
+        alert("Last Name is required.");
+        isValid = false;
+    } else if (!phoneNumber.value.trim()) {
+        alert("Phone Number is required.");
+        isValid = false;
+    } else if (!email.value.trim()) {
+        alert("Email Address is required.");
+        isValid = false;
+    } else if (!message.value.trim()) {
+        alert("Message is required.");
+        isValid = false;
+    }
+
+    if (isValid) {
+        alert("Form Submitted Successfully");
+        alert("Farith will contact u soon");
+        document.getElementById("myForm").submit(); 
+    }
+});
